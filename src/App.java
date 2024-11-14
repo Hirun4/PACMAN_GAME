c99a4adf28b1ac01894c6a55ec5c715138735331
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-14 16:00:20 +0530

    exit

--- src/App.java
+++ src/App.java
@@ -12,7 +12,8 @@ public class App {
         JFrame frame = new JFrame("Pac Man");
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
-        
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
     }
