56d7bb8de531fb683acdaa988c24d586701abe4d
MSI <hirun08072002@gmail.com>
2024-11-14 13:30:49 +0530

    row count

--- src/App.java
+++ src/App.java
@@ -1,7 +1,9 @@
+import javax.swing.JFrame;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int rowCount = 21;
     }
 }
 
