488ca22b24172a23afb8e8ccf3cf8a37a1bb0232
MSI <hirun08072002@gmail.com>
2024-11-14 14:18:44 +0530

    tile

--- src/App.java
+++ src/App.java
@@ -5,6 +5,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         int rowCount = 21;
         int columnCount = 19;
+        int tileSize = 32;
+        
 
     }
 }
