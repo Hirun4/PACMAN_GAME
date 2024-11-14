d7ffd488f38ecc526dadd7eb62126d0359270773
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-14 16:46:45 +0530

    changes

--- src/PacMan.java
+++ src/PacMan.java
@@ -4,7 +4,11 @@ import java.util.HashSet;
 import java.util.Random;
 import javax.swing.*;
 
-
-public class PacMan extends JPanel{
+public class PacMan extends JPanel {
+    int rowCount = 21;
+    int columnCount = 19;
+    int tileSize = 32;
+    int boardWidth = columnCount * tileSize;
+    int boardHeight = rowCount * tileSize;
 
 }
